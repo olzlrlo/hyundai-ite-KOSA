88625f89750d54f6af69e7767b735b4e3f2ba4b9
JIEUN LEE <89925581+olzlrlo@users.noreply.github.com>
2022-02-28 17:34:28 +0900

    Delete JAVA/Animal_Simulation/com/olzlrlo directory

--- JAVA/Animal_Simulation/com/olzlrlo/Canine.java
+++
@@ -1,8 +0,0 @@
-package com.olzlrlo;
-
-public abstract class Canine extends Animal{
-
-    void roam(){
-        System.out.println("개과답게 돌아다닙니다.");
-    }
-}
--- JAVA/Animal_Simulation/com/olzlrlo/Vet.java
+++
@@ -1,4 +0,0 @@
-package com.olzlrlo;
-
-public class Vet {
-}
